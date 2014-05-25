995645a3f9958b98682ceb367f2ce53f0ea613f3
Tim Yang <yzgogo2011@sina.com>
2014-05-26 01:25:57 +0800

    draw popup page

--- client/WebContent/js/init.js
+++ client/WebContent/js/init.js
@@ -58,14 +58,13 @@ var renderSentence = function(sentence) {
         var sentenceArea = $('<div class="sentenceArea"></div>').appendTo(movie);
         $(elem.sentence).each(function(index, elem) {
             $('<div style="cursor: pointer;" class="sentence">' + elem.sentence + '----from ' + elem.startTime + ' seconds' + '----to ' + elem.endTime + ' seconds<div>').appendTo(sentenceArea).click(function(){    
-                openVideo('panda.mp4', elem.startTime, elem.endTime);
+                openVideo('a.mp4', elem.startTime, elem.endTime);
             });
             $('<div class="sentence" style="">' + elem.chinese + '</div>').appendTo(sentenceArea);
-            $('<div class="operation" style="color: #777; margin-top: 10px;margin-bottom: 34px;"><div style="margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/fav.png)"></div><span>收藏例句</span><div style="margin-left: 15px;margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/play.png)"></div><span style="cursor: pointer;" onclick="openVideo(\'panda.mp4\', ' + elem.startTime + ',' + elem.endTime+')">播放例句</span></div>').appendTo(sentenceArea);
+            $('<div class="operation" style="color: #777; margin-top: 10px;margin-bottom: 34px;"><div style="margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/fav.png)"></div><span>收藏例句</span><div style="margin-left: 15px;margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/play.png)"></div><span style="cursor: pointer;" onclick="openVideo(\'a.mp4\', ' + elem.startTime + ',' + elem.endTime+'); showContentPanelSentence();">播放例句</span></div>').appendTo(sentenceArea);
         });
         $('<div style="height: 1px; background-color: #333;width: 100%;"></div>').appendTo($("#sentenceArea"));
     });
-    
 };
 
 function search() {
@@ -73,8 +72,24 @@ function search() {
     renderSentence(sentence);
 }
 
-function hideGrayPanel(){
-    $("#graypanel").hide();
+function showContentPanelSentence(){
+    var sentence = parseJson();
+    renderPopupSentence(sentence);
+}
+
+var renderPopupSentence = function (sentence){
+    $('#content_right').empty();
+    $(sentence).each(function(index, elem) {
+        var sentenceArea = $('#content_right');
+        $(elem.sentence).each(function(index, elem) {
+            $('<div style="cursor: pointer;" class="popupsentence">' + elem.sentence + '----from ' + elem.startTime + ' seconds' + '----to ' + elem.endTime + ' seconds<div>').appendTo(sentenceArea).click(function(){    
+                openVideo('a.mp4', elem.startTime, elem.endTime);
+            });
+            $('<div class="popupsentence" style="">' + elem.chinese + '</div>').appendTo(sentenceArea);
+            $('<div class="operation" style="color: #aaa; margin-top: 10px;margin-bottom: 34px; padding-left:16px;"><div style="margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/fav.png)"></div><span>收藏例句</span><div style="margin-left: 15px;margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/play.png)"></div><span style="cursor: pointer;" onclick="openVideo(\'a.mp4\', ' + elem.startTime + ',' + elem.endTime+')">播放例句</span></div>').appendTo(sentenceArea);
+        });
+        $('<div style="height: 1px; background-color: #333;width: 100%;"></div>').appendTo($("#sentenceArea"));
+    });
 }
 
 function openVideo(videoName, startSec, endSec) {
