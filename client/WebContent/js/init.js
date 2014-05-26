d85863e31fdc5b616c1ac6d0282cb13b9cc1d850
dagouaofei@sina.com <2011qpAL>
2014-05-26 17:37:01 +0800

    Update main page.

--- client/WebContent/js/init.js
+++ client/WebContent/js/init.js
@@ -2,7 +2,7 @@ var timeoutFn = function() {
     clearTimeout(timeoutFn);
     $( "#sampleVideo")[0].pause();
 };
-var json = '';
+var json = '', resultSentence = null;
 
 $(function() {
     var renderSentence = function(sentence) {
@@ -54,26 +54,31 @@ var parseJson = function() {
 var renderSentence = function(sentence) {
     $("#sentenceArea").empty();
     $(sentence).each(function(index, elem) {
-        var movie = $('<div class="movie"><div style="float: left;padding: 20px;"><div class="thumbnail"></div><div style="color: #333; margin-top:12px;">' + elem.area + '</div></div></div>').appendTo($("#sentenceArea"));
+        var movieIndex = index;
+        var movie = $('<div class="movie"><div style="float: left;padding: 20px;"><div class="thumbnail"></div><div style="color: #333; margin-top:12px;">' + elem.name + '</div></div></div>').appendTo($("#sentenceArea"));
         var sentenceArea = $('<div class="sentenceArea"></div>').appendTo(movie);
         $(elem.sentence).each(function(index, elem) {
-            $('<div style="cursor: pointer;" class="sentence">' + elem.sentence + '----from ' + elem.startTime + ' seconds' + '----to ' + elem.endTime + ' seconds<div>').appendTo(sentenceArea).click(function(){    
-                openVideo('a.mp4', elem.startTime, elem.endTime);
+            $('<div style="cursor: pointer;" class="sentence">' + elem.sentence + '<div>').appendTo(sentenceArea).click(function(){    
+                openVideo(sentence, movieIndex, index);
+                //openVideo('a.mp4', elem.startTime, elem.endTime);
             });
             $('<div class="sentence" style="">' + elem.chinese + '</div>').appendTo(sentenceArea);
-            $('<div class="operation" style="color: #777; margin-top: 10px;margin-bottom: 34px;"><div style="margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/fav.png)"></div><span>收藏例句</span><div style="margin-left: 15px;margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/play.png)"></div><span style="cursor: pointer;" onclick="openVideo(\'a.mp4\', ' + elem.startTime + ',' + elem.endTime+'); showContentPanelSentence();">播放例句</span></div>').appendTo(sentenceArea);
+            $('<div class="operation" style="color: #777; margin-top: 4px;margin-bottom: 34px;"><div style="margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/fav.png)"></div><span>收藏例句</span><div style="margin-left: 15px;margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/play.png)"></div><span style="cursor: pointer;" onclick="openVideo(\'a.mp4\', ' + elem.startTime + ',' + elem.endTime+'); showContentPanelSentence();">播放例句</span></div>').appendTo(sentenceArea);
         });
         $('<div style="height: 1px; background-color: #333;width: 100%;"></div>').appendTo($("#sentenceArea"));
     });
 };
 
 function search() {
+    if(!$('#keyword').val() || $('#keyword').val().length <= 2) {
+        return;
+    }
     var sentence = parseJson();
     renderSentence(sentence);
 }
 
 function showContentPanelSentence(){
-    var sentence = parseJson();
+    sentence = parseJson();
     renderPopupSentence(sentence);
 }
 
@@ -90,9 +95,16 @@ var renderPopupSentence = function (sentence){
         });
         $('<div style="height: 1px; background-color: #333;width: 100%;"></div>').appendTo($("#sentenceArea"));
     });
+};
+
+function searchKeyDown(event) {
+    e = event ? event :(window.event ? window.event : null); 
+    if(e.keyCode==13){ 
+        search();
+    } 
 }
 
-function openVideo(videoName, startSec, endSec) {
+function openVideo(result, movieIndex, sentenceIndex) {
     $("#graypanel").show();
     // $( "#dialog" ).show().dialog({
     //     width: "626px", 
@@ -102,7 +114,7 @@ function openVideo(videoName, startSec, endSec) {
     //         $("#graypanel").hide();
     //     }});
     $( "#sampleVideo").empty();
-    $( "#sampleVideo").append("<source src='" + videoName + "' type='video/mp4'>");
+    $( "#sampleVideo").append("<source src='" + result[movieIndex].name + ".mp4' type='video/mp4'>");
     $( "#sampleVideo").on('loadedmetadata', function() {
         var sampleVideo = this;
         sampleVideo.currentTime = startSec;
