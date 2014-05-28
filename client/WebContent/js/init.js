e280a9cc77e729e2ef0b845f93478ce46f4beda7
Tim Yang <yzgogo2011@sina.com>
2014-05-29 00:08:01 +0800

    fix conflict

--- client/WebContent/js/init.js
+++ client/WebContent/js/init.js
@@ -66,7 +66,7 @@ var renderSentence = function(sentence) {
             var operation = $('<div class="operation" style="color: #777; margin-top: 4px;margin-bottom: 34px;"><div style="margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/fav.png)"></div><span>收藏例句</span><div style="margin-left: 15px;margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/play.png)"></div></div>').appendTo(sentenceArea);
             $('<span style="cursor: pointer;">播放例句</span>').appendTo(operation).click(function(){    
                 openVideo(sentence, movieIndex, index);
-                //showContentPanelSentence();
+                showContentPanelSentence(movieIndex);
             });;
         });
         $('<div style="height: 1px; background-color: #333;width: 100%;"></div>').appendTo($("#sentenceArea"));
@@ -81,18 +81,19 @@ function search() {
     renderSentence(sentence);
 }
 
-function showContentPanelSentence(){
+function showContentPanelSentence(movieIndex){
     sentence = parseJson();
     renderPopupSentence(sentence);
 }
 
-var renderPopupSentence = function (sentence){
+var renderPopupSentence = function (sentence, movieIndex){
+    var result = sentence[movieIndex];
     $('#content_right').empty();
-    $(sentence).each(function(index, elem) {
+    $(result).each(function(index, elem) {
         var sentenceArea = $('#content_right');
-        $(elem.sentence).each(function(index, elem) {
+        $(elem.result).each(function(index, elem) {
             $('<div style="cursor: pointer;" class="popupsentence">' + elem.sentence + '----from ' + elem.startTime + ' seconds' + '----to ' + elem.endTime + ' seconds<div>').appendTo(sentenceArea).click(function(){    
-                openVideo('a.mp4', elem.startTime, elem.endTime);
+                openVideo(result, movieIndex, elem.endTime);
             });
             $('<div class="popupsentence" style="">' + elem.chinese + '</div>').appendTo(sentenceArea);
             $('<div class="operation" style="color: #aaa; margin-top: 10px;margin-bottom: 34px; padding-left:16px;"><div style="margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/fav.png)"></div><span>收藏例句</span><div style="margin-left: 15px;margin-right: 5px;display: inline-block;width:16px;height:16px;background-image:url(assets/play.png)"></div><span style="cursor: pointer;" onclick="openVideo(\'a.mp4\', ' + elem.startTime + ',' + elem.endTime+')">播放例句</span></div>').appendTo(sentenceArea);
